595af7a5c2ab271b69f0422c9b496c231ab31fb4
shaan.lee <dev.sehyeon@gmail.com>
2023-09-20 16:23:49 +0900

    cover specify cases

--- google_vision.py
+++ google_vision.py
@@ -1,4 +1,5 @@
 "Google Vision api Class"
+import base64
 import requests
 
 
@@ -12,45 +13,34 @@ class GoogleVision:
         self.request_endpoint = "https://vision.googleapis.com/v1/images:annotate"
         self.request_url = self.request_endpoint + "?" + "key=" + self.api_key
 
-    def get_features(self, feature_type: str, max_results: int = 10, model: str = None):
-        """
-        Args:
-            feature_type (str): type of feature [TYPE_UNSPECIFIED,FACE_DETECTION,LANDMARK_DETECTION,LOGO_DETECTION,LABEL_DETECTION,TEXT_DETECTION,DOCUMENT_TEXT_DETECTION,SAFE_SEARCH_DETECTION,IMAGE_PROPERTIES,CROP_HINTS,WEB_DETECTION,PRODUCT_SEARCH,OBJECT_LOCALIZATION]
-            max_results (int, optional): result max count, Defaults to 10.
-            model (str, optional): using model name, Defaults to None.
-
-        Returns:
-            list : contains features dictionary
-        """
-        features = {"type": feature_type, "maxResults": max_results, "model": model}
+    def __get_features(
+        self, feature_type: str, max_results: int = 10, model: str = None
+    ):
+        features = {
+            "type": feature_type.upper(),
+            "maxResults": max_results,
+            "model": model.upper(),
+        }
         return [features]
 
-    def get_image_object(self, img_source: str = "", content: bytes = b""):
-        """
-        Args:
-            img_source (str, optional): using image uri, Defaults to "".
-            content (bytes, optional): using image bytes, Defaults to b"".
-
-        Returns:
-            dictionary : contains decoded content, image uri
-        """
-        image = {
-            "content": content.decode("base64"),
-            "source": {"imageUri": img_source},
-        }
-        return image
+    def __get_image_object(self, img_source: str or bytes):
+        if isinstance(img_source, bytes):
+            return {"content": base64.b64encode(img_source).decode("utf-8")}
+        return {"source": {"imageUri": img_source}}
 
     def request_api(
         self,
-        img_source: str = "",
-        content: bytes = b"",
+        img_source: str or bytes,
         feature_type: str = "WEB_DETECTION",
+        max_result: int = 10,
+        time_out: int = 10,
     ):
         """
         Args:
-            img_source (str, optional): using image uri, Defaults to "".
-            content (bytes, optional): using image bytes, Defaults to b"".
-            feature_tyep (str, optional): type of feature, Defaults to "WEB_DETECTION".
+            img_source (str or bytes, require): using image uri or image bytes.
+            feature_type (str, optional): type of feature, Defaults to "WEB_DETECTION".
+            max_result (int, optional): count of results, Defaults to 10.
+            time_out (int, optional): second of request, Defaults to 10.
 
         Returns:
             response : result of api reqeusts
@@ -58,13 +48,11 @@ class GoogleVision:
         data = {
             "requests": [
                 {
-                    "image": self.get_image_object(
-                        img_source=img_source, content=content
-                    ),
-                    "features": self.get_features(
-                        feature_type=feature_type, max_results=0, model=None
+                    "image": self.__get_image_object(img_source=img_source),
+                    "features": self.__get_features(
+                        feature_type=feature_type, max_results=max_result, model=None
                     ),
                 },
             ]
         }
-        return requests.post(timeout=10, url=self.request_url, json=data)
+        return requests.post(timeout=time_out, url=self.request_url, json=data)
